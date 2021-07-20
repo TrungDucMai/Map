b53d64ec4c814bdd0d8ef0a0b20ad597299f83c5
TrungDucMai <trungmaiduc196@gmail.com>
2021-07-20 11:34:24 +0700

    'tomoi'

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("Smith", 30);
+        hashMap.put("Anderson", 31);
+        hashMap.put("Lewis", 29);
+        hashMap.put("Cook", 29);
+        System.out.println("Display entries in HashMap");
+        System.out.println(hashMap + "\n");
+
+        Map<String, Integer> treeMap = new TreeMap<>(hashMap);
+        System.out.println("Display entries in ascending order of key");
+        System.out.println(treeMap);
+
+
+    }
+}
